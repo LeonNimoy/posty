cde8799e7e1d81498c9f9abd5ab388898ab00cce
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-14 06:02:43 -0300

    feat:register blade updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,6 +9,31 @@
                 <input type="text" name="name" id="name" placeholder="Your name"
                     class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
             </div>
+            <div class="mb-4">
+                <label for="username" class="sr-only">Username</label>
+                <input type="text" name="username" id="username" placeholder="Username"
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+            </div>
+            <div class="mb-4">
+                <label for="email" class="sr-only">Email</label>
+                <input type="text" name="email" id="email" placeholder="Your email"
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+            </div>
+            <div class="mb-4">
+                <label for="password" class="sr-only">Email</label>
+                <input type="password" name="password" id="password" placeholder="Choose password"
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+            </div>
+            <div class="mb-4">
+                <label for="password_confirmation" class="sr-only">Email</label>
+                <input type="password" name="password_confirmation" id="password_confirmation"
+                    placeholder="Repeat your password" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+            </div>
+
+            <div>
+                <button type="submit"
+                    class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Register</button>
+            </div>
         </form>
     </div>
 </div>
