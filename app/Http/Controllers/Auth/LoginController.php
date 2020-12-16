0a0b26145bcb837d733107e34f1fc81e11cb8eb0
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-16 06:31:26 -0300

    feat:login controller created

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        Auth::attempt($request->only('email', 'password'));
+
+        return redirect()->route('dashboard');
+    }
+}
