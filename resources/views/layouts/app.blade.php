4d02a1c78b32596a58db77aec3153201cfdc7e73
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-16 06:33:20 -0300

    feat:app and register blade updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 <body class="bg-gray-200">
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
+
             <li>
                 <a href="" class="p-3">Home</a>
             </li>
@@ -22,20 +23,27 @@
             <li>
                 <a href="" class="p-3">Post</a>
             </li>
+
         </ul>
         <ul class="flex items-center">
+            @auth
             <li>
                 <a href="" class="p-3">Peter Parker</a>
             </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
+            </li>
+            @endauth
+
+            @guest
+
             <li>
                 <a href="" class="p-3">Login</a>
             </li>
             <li>
                 <a href="{{route('register')}}" class="p-3">Register</a>
             </li>
-            <li>
-                <a href="" class="p-3">Logout</a>
-            </li>
+            @endguest
         </ul>
     </nav>
     @yield('content')
