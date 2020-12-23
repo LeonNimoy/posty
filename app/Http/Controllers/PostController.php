b38179785efabb703a8210057f424f0c231fd089
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-23 13:57:04 -0300

    store method for post controller created

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class PostController extends Controller
 {
     public function index()
     {
         return view('posts.index');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'body' => 'required',
+        ]);
+
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
     }
 }
