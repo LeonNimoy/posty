13e70d6b67813eb7514e6fa34cad16382035e8ed
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-30 15:02:55 -0300

    test: create constructor and destroy method for postLikeController - update post model, index.blade and web routes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -32,17 +32,19 @@
                 </div>
 
                 <div class="flex items-center">
+                @if(!$post->likedBy(auth()->user()))
 
-                <form action="{{route('posts.likes', $post->id)}}" method="post" class="mr-1">
+                <form action="{{route('posts.likes', $post)}}" method="post" class="mr-1">
                     @csrf
                 <button type="submit" class="text-blue-500">Like</button>
                 </form>
-
-                <form action="" method="post" class="mr-1">
+                @else
+                <form action="{{route('posts.likes', $post)}}" method="post" class="mr-1">
                     @csrf
+                    @method('DELETE')
                 <button type="submit" class="text-blue-500">Unlike</button>
                 </form>
-
+                @endif
                 <span>{{$post->likes->count()}} {{Str::plural('like', $post->likes->count())}}</span>
 
                 </div>
