f559c465a1770692d75e0b12f122c707702782fd
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-04 09:50:13 -0300

    refactor: ensure to equal load the post queries

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -32,6 +32,7 @@
                 </div>
 
                 <div class="flex items-center">
+                @auth
                 @if(!$post->likedBy(auth()->user()))
 
                 <form action="{{route('posts.likes', $post)}}" method="post" class="mr-1">
@@ -45,6 +46,7 @@
                 <button type="submit" class="text-blue-500">Unlike</button>
                 </form>
                 @endif
+                @endauth
                 <span>{{$post->likes->count()}} {{Str::plural('like', $post->likes->count())}}</span>
 
                 </div>
